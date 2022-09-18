e0c426dbc5d5dcc42a1ab156856560cd42e3cd0a
keltsjsx <kelts.jsx@gmail.com>
2022-09-18 21:32:25 +0700

    Array and Slices

--- main.go
+++ main.go
@@ -13,6 +13,9 @@ func main() {
 	fmt.Printf("We have total of %v and %v are still available\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your ticket to attend")
 
+	// var bookings []string
+	bookings := []string{}
+
 	var firstName string
 	var lastName string
 	var email string
@@ -29,7 +32,17 @@ func main() {
 	fmt.Scan(&userTicket)
 
 	remainingTickets = remainingTickets - userTicket
+	// bookings[0] = firstName + " " + lastName
+	bookings = append(bookings, firstName+" "+lastName)
 
 	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v \n", firstName, lastName, userTicket, email)
 	fmt.Printf("%v tickets remaining for %v \n", remainingTickets, conferenceName)
+
+	// ====== Array Informations ======
+	// fmt.Printf("The whole array: %v \n", bookings)
+	// fmt.Printf("The first element array: %v \n", bookings[0])
+	// fmt.Printf("Array type: %T \n", bookings)
+	// fmt.Printf("Array length: %v \n", len(bookings))
+
+	fmt.Printf("These are all our bookings: %v \n", bookings)
 }
